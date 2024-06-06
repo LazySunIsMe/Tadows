ba612d2ee56e69ea76a6f6ec310bd0623a0ef5bd
LazySunIsMe <2825258839@qq.com>
2024-06-06 15:33:48 +0800

    sdfsdfsdff

---
+++ src/main/java/com/example/tadows_tt/TadowsTtApplication.java
@@ -0,0 +1,13 @@
+package com.example.tadows_tt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadowsTtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadowsTtApplication.class, args);
+    }
+
+}
